130a2b3b65ba791c6b364696ea6370b38c307a93
LoKiJ07 <louizkylaspona@gmail.com>
2025-03-10 08:53:20 +0800

    Updates

--- assets/swipe-back-legacy-DUU3odYM.js
+++ assets/swipe-back-legacy-DUU3odYM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-mDYqeEkf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CJOe74-2.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
