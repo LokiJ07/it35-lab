dd125271478d048f3753b4b90dfd7b3bec43337a
LoKiJ07 <louizkylaspona@gmail.com>
2025-03-13 08:50:46 +0800

    Updates

--- assets/ios.transition-legacy-faiLkaar.js
+++ assets/ios.transition-legacy-faiLkaar.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CJOe74-2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BhQcDWfG.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
