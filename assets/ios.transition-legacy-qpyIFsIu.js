e9a9d34e927fe6948e6e98999eb9382ddde4cccd
Loki07 <louizkylaspona@gmail.com>
2025-04-28 15:53:57 +0800

    Updates

--- assets/ios.transition-legacy-qpyIFsIu.js
+++ assets/ios.transition-legacy-qpyIFsIu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BC0MfCxl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DzsBVw0p.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-B4CD4eiO.js
+++ assets/md.transition-legacy-B4CD4eiO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BC0MfCxl.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DzsBVw0p.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
